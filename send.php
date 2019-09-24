cdd1764bf4d9b3779d3ef69f06337290f93b7f6b
sokolik1983-hub <lionsbet@outlook.com>
2019-09-24 16:58:55 +0300

    add start footer

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("sokolik1983@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: 4867242@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
