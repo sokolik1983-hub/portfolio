7a4678a7e06a40ba09707f7bdf2611f38fde6b77
sokolik1983-hub <lionsbet@outlook.com>
2019-11-13 02:08:33 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 // инициировать wow
 var wow = new WOW();
 wow.init();
@@ -183,3 +185,27 @@ $(function(){
         e.preventDefault();
     });
 });
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop:true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs,
+    },
+});
+
+
